df2927502cbb77421c48640eb02bba20bf52ff1a
camu-al <alexcammar04@gmail.com>
2024-11-07 10:23:06 +0100

    Añadir el login

---
+++ login.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<body>
+    <?php
+        $user = $_POST["user"];
+        $pwd = $_POST["password"];
+
+        echo "El teu usurai és " .$user." i la teua contrasenya és ".$pwd; 
+    ?>
+</body>
+</html>
